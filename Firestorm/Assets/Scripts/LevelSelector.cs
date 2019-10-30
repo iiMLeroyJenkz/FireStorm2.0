71faa9b053cd22231bbb7c63c72978f3e9d54ca4
alexh16 <50339946+alexh16@users.noreply.github.com>
2019-10-30 16:27:17 -0400

    Main Menu & Level Selector

---
+++ Firestorm/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{ 
+
+	
+
+    public void MainMenu ()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void levelOne ()
+    {
+        SceneManager.LoadScene( 2);
+    }
+
+
+
+    public void QuitGame ()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    
+}
