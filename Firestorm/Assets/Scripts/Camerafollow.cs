6dbb16d61927dc2c966a2d9bc244f5ebcc40523d
iiMLeroyJenkz <56455387+iiMLeroyJenkz@users.noreply.github.com>
2019-11-05 12:54:57 -0500

    added in Animations for shotgun , submachine gun and the full pistol anim, also added in a camera control script
    ta da

---
+++ Firestorm/Assets/Scripts/Camerafollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerafollow : MonoBehaviour
+{
+    private Transform playerTransform;
+
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    
+    void LateUpdate()
+    {
+        //camera's position in a variable temp
+        Vector3 temp = transform.position;
+
+        temp.x = playerTransform.position.x;
+        temp.y = playerTransform.position.y;
+
+        transform.position = temp;
+
+        
+    }
+}
