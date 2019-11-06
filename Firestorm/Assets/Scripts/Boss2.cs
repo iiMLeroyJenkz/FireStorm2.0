77a28734c690d0f4d07cbb78e0e1f3c1e87a8a1a
MrNiceGuy12312 <52978365+MrNiceGuy12312@users.noreply.github.com>
2019-11-06 14:27:08 -0500

    boss shootin at the payer and and following/retreating/dying boss

---
+++ Firestorm/Assets/Scripts/Boss2.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss2 : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    public float timeBtwShots;
+    public float StartTimeBtwShots;
+
+    public GameObject projectile;
+    public Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = StartTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if(Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+        if(timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = StartTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
